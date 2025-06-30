747e180b4e0e73a56d61191ef0cd00399309fd26
Aurnab <zubayeraurnab@gmail.com>
2025-06-30 23:15:21 +0600

    learned about loops

---
+++ app.py
@@ -0,0 +1,10 @@
+x = input("x: ")
+y = int(x)+1
+
+# if y >= 3:
+#     print("hell")
+# else:
+#     print("haven")
+
+for number in range(9):
+    print(number)
---
+++ loops.py
@@ -0,0 +1,26 @@
+successfull = ''
+
+# for number in range(1, 10):
+#     print("attempt")
+#     if successfull:
+#         print("success")
+#         break
+# else:
+#     print("Faild attemted 10  times")
+
+
+# for x in range(6):
+#     for y in range(5):
+#         print(f" {x}->{y}")
+
+
+# while successfull.lower() != 'quit':
+#     successfull = input(">")
+#     print("Eccho", successfull)
+
+while True:
+    print("zzzzz")
+    successfull = input(">")
+    if successfull.lower() == "quit":
+        print("end of an era... ")
+        break

7785fb9bfcdd1a58a9c71742e4054981e2f13b08
Aurnab <zubayeraurnab@gmail.com>
2025-07-03 00:01:27 +0600

    .

---
+++ functions.py
@@ -0,0 +1,13 @@
+temp = 34
+
+# if temp > 30:
+#     print("high  temp")
+# else:
+#     print("Low temp")
+
+# print(temp)
+
+
+message = "Low temp " if temp < 30 else "High temp"
+
+print(message)
---
+++ logicalOperaters.py
@@ -0,0 +1,7 @@
+hign_incom = True
+good_creadit = False
+
+if hign_incom and good_creadit:
+    print("good to go ")
+else:
+    print("STOP")
